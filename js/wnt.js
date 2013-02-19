5dcb7db8301d919e00d45868bafb9c22140559ae
Taylor Johnson <webninjataylor@gmail.com>
2013-02-19 16:33:42 -0500

    Removed alert and commented out Google Analytics for project overrides

--- js/wnt.js
+++ js/wnt.js
@@ -1,7 +1,5 @@
-/******** WEB NINJA TAYLOR ********/
-alert('WTF CODEMONEY?!');
-
 /******** GOOGLE ANALYTICS ********/
+/*
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-31306164-2']);
 _gaq.push(['_trackPageview']);
@@ -11,6 +9,7 @@ _gaq.push(['_trackPageview']);
     ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+*/
 
 
 /******** LOCK WINDOW ********/
