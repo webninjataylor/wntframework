c77634f90b9ea0f8b55892209a2522f852363351
Taylor Johnson <webninjataylor@gmail.com>
2013-03-14 16:22:54 -0400

    Added base CSS and JS for new projects

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//@codekit-prepend "jquery-1.9.0.min.js";
+//@codekit-prepend "modernizr.min.js";
+//@codekit-prepend "jquery.dataTables.min.js";
+//@codekit-prepend "wnt.js";
+
+
+
+/******** GOOGLE ANALYTICS ********/
+/*** INSERT HERE ***/
