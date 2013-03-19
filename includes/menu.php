21fa9a2f9aa55fa99c2fdd3ad0133a4d65b68574
Taylor Johnson <webninjataylor@gmail.com>
2013-03-19 15:15:35 -0400

    Removed data-path attribute from menu links

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,5 @@
 <menu>
-    <li><a href="index.php" data-path="/index.php">Home</a></li>
-    <li><a href="about.php" data-path="/about.php">About</a></li>
-    <li><a href="contactus.php" data-path="/contactus.php">Contact Us</a></li>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="contactus.php">Contact Us</a></li>
 </menu>
