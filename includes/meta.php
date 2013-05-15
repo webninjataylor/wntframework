8308f2797540a8a8c345c7e7f196199842faa845
Taylor Johnson <webninjataylor@gmail.com>
2013-05-15 15:31:19 -0400

    No caching for mobile testing

--- includes/meta.php
+++ includes/meta.php
@@ -1,3 +1,6 @@
+<!-- JUST FOR MOBILE TESTING -->
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Expires" content="-1">
 <!-- Mobile Settings -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
 <meta name="apple-mobile-web-app-capable" content="yes" />
