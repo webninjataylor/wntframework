b3afb1bcae00c73c66eeebcd413a57e06295b55a
Taylor Johnson <webninjataylor@gmail.com>
2013-05-22 14:01:11 -0400

    Added JS unit testing to framework

--- includes/menu.php
+++ includes/menu.php
@@ -2,4 +2,5 @@
     <li><a href="index.php">Home</a></li>
     <li><a href="about.php">About</a></li>
     <li><a href="contactus.php">Contact Us</a></li>
+    <li><a href="tests.php">JS Unit Tests</a></li>
 </menu>
